d794274711d52503f7ca62b8bfed8b149667050a
程逸涵 <117724328+dongdafarmer@users.noreply.github.com>
2024-11-07 19:51:56 +0800

    前后端项目创建

---
+++ melon-carpooling-platform/melon-carpooling-base/src/main/java/nwpu/software/MelonCarpoolingBaseApplication.java
@@ -0,0 +1,13 @@
+package nwpu.software;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelonCarpoolingBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MelonCarpoolingBaseApplication.class, args);
+    }
+
+}
